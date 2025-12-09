1694d958991199142860e46d6aa112e69e200ecb
Brikt2 <briktsunde@gmail.com>
2025-12-09 10:16:35 +0100

    Implement Flask app with timeplan API
    Added Flask routes for home and timeplan API.

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
 
+from flask import Flask, jsonify, render_template
+import subprocess
+
+app = Flask(__name__)
+
+timeplan_path="/home/im/mp-bilder/timeplan.jpg"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/api/timeplan", methods=["POST"])
+def timeplan():
+        subprocess.run (["pkill", "feh"], stderr=subprocess.DEVNULL)
+        subprocess.Popen(["feh", "-F", timeplan_path])
+        return jsonify({"status": "ok"})
+
+
+if __name__=="__main__":
+        app.run(host="0.0.0.0", port=5000)
+
+
